d2768c51c1907da15297cf6e305ae0ddbc3782c5
Exequiel-Ansaldi <exeansaldi0@gmail.com>
2024-11-04 22:40:30 -0300

    agregando

--- tp5_enviroment/Punto_villanos/main.py
+++ tp5_enviroment/Punto_villanos/main.py
@@ -2,7 +2,9 @@ from Personaje import Personaje
 class Ejecutable:
     @staticmethod
     def main():
-        archivo = 'superheroes_villanos.txt'
+        archivo = 'superheroes_villanos.txt' 
+        #Con el nombre del archivo txt solamente se puede ejecutar desde el directorio Punto_Villano y desde la terminal
+        #Con la ruta absoluta del archivo se puede ejecutar desde cualquier directorio
         lineas = Personaje.leer_archivo(archivo)
         personajes = Personaje.clasificar_personajes(lineas)
         for personaje in personajes:
